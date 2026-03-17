0d8c680085e5f50df4c4d8954f807689baf119c0
DESKTOP-NHSOKD0\PC <wnddnjs1886@naver.com>
2026-03-17 23:14:48 +0900

    나의 작업물을 올릴려 하다가 팀원의 패치 발견 > 현재시점의 작업물에서 내브랜치를 새로 파서 커밋을 일단하고 패치풀을 한다

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -8,13 +8,12 @@ AMyActor::AMyActor()
 	PrimaryActorTick.bCanEverTick = true;
 
 }
-//갈등을 해소하고 합의된 내용으로 작업중... >> 옛작업 브랜치 run을 버리고
-//새로운 브랜치 new를 파서 합의된 내용으로 새작업중...
+
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~~~"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play~"));
 
 }
 
@@ -22,7 +21,7 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!~~~~"));
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
 
 }
 
