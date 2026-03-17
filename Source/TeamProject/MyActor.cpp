8ccb665825b520c26b6bf9ac45b62e891018becf
DESKTOP-NHSOKD0\PC <wnddnjs1886@naver.com>
2026-03-17 22:19:18 +0900

    아래를 물결표로 수정한 내작업

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -8,7 +8,8 @@ AMyActor::AMyActor()
 	PrimaryActorTick.bCanEverTick = true;
 
 }
-
+//갈등을 해소하고 합의된 내용으로 작업중... >> 옛작업 브랜치 run을 버리고
+//새로운 브랜치 new를 파서 합의된 내용으로 새작업중...
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -21,7 +22,7 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!!"));
-
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!~~~"));
+	//다시보니 물결표로 수정해야함.
 }
 
